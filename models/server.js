cc1a12788ef189c8174059f85f5cf0d36924c33d
Luis Gustavo <luisgustavoramirez17@gmail.com>
2021-05-29 17:49:55 -0500

    Primer commit

---
+++ models/server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+var cors = require('cors');
+
+
+class Server {
+
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT;
+        this.usuariosPath='/api/usuarios';
+
+        //MiddleWares
+        this.middlewares();
+
+        //Rutas de mi aplicacion 
+        this.routes();
+    }
+
+    middlewares(){
+        //CORS
+        this.app.use(cors());
+
+        //Lectura y Parseo del Body
+        this.app.use(express.json());
+
+        //Direcctorio Publico
+        this.app.use(express.static('public'));
+    }
+
+    routes() {
+        this.app.use(this.usuariosPath,require('../routes/usuarios'))
+    }
+
+    listen() {
+        this.app.listen(this.port, () => {
+            console.log('Servidor corriendo en el Puerto: ', this.port);
+        });
+    }
+
+
+}
+
+module.exports = Server;
